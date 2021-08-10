7b502a5e09e5c51f331559646ceeb370694c4bf8
Mike Johnson <mijohns@microsoft.com>
2021-08-10 15:23:18 -0600

    Added vnet, subnet, and rg to packer

--- .azure/pipelines/scripts/build-image.ps1
+++ .azure/pipelines/scripts/build-image.ps1
@@ -50,6 +50,9 @@ packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
                 -var "subscription_id=$SubscriptionId" `
                 -var "temp_resource_group_name=$TempResourceGroupName" `
                 -var "tenant_id=$TenantId" `
+                -var "virtual_network_name=$VirtualNetworkName" `
+                -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
+                -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
                 -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
                 -var "allowed_inbound_ip_addresses=$AgentIp" `
                 $TemplatePath `
