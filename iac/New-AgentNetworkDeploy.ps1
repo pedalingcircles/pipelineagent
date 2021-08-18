72358518fac8e942e3b7f4b21e420579d0b69e34
Mike Johnson <mijohns@microsoft.com>
2021-08-18 14:31:46 -0600

    renamed to agents iac

--- iac/New-AgentNetworkDeploy.ps1
+++ iac/New-AgentNetworkDeploy.ps1
@@ -6,6 +6,6 @@ az group create --name $resourceGroupName --location $location
 
 az deployment group create `
     --resource-group $resourceGroupName `
-    --template-file agent-network.bicep `
-    --parameters agent-network.parameters.sbx.json
+    --template-file agents.bicep `
+    --parameters agents.parameters.sbx.json
     
