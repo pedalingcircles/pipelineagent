3fe398dfebbf2e0d96f8c171921ab0b9257f9ebe
Mike Johnson <mijohns@microsoft.com>
2021-08-20 18:43:41 -0600

    Changed folder from iac to templates

---
+++ scripts/New-AgentImages.ps1
@@ -0,0 +1,16 @@
+
+[CmdletBinding()]
+param(
+    [String] [Parameter (Mandatory=$true)] $ResourceGroupName,
+    [String] [Parameter (Mandatory=$true)] $Location,
+    [String] [Parameter (Mandatory=$true)] $TemplateFilePath,
+    [String] [Parameter (Mandatory=$true)] $TemplateParameterFilePath
+)
+
+az group create --name $ResourceGroupName --location $Location
+
+az deployment group create `
+    --resource-group $ResourceGroupName `
+    --template-file $TemplateFilePath `
+    --parameters $TemplateParameterFilePath `
+    --no-prompt
