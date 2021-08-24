2ebdb9bcaf4f51120f09c350d10872a017215fda
Mike Johnson <54952613+pedalingcircles@users.noreply.github.com>
2021-08-23 20:55:28 -0600

    Updated Contoso provisioning to include VM deployment simple case (#7)
    * Changed folder from iac to templates
    
    * Updated variable template with new values to support Contoso sample
    
    * Added vnet and subnet
    
    * Removed Clean363763763763763763763
    
    * Updates from actions/virtual-environments
    
    this is a complete folder update and updated to commit: 20584a43947912f28b9577384d30bc67dcd933f7
    
    * updates on agent provisioning
    
    Adding placeholder for setup scripts
    
    * Squashed '.virtual-environments/' changes from c706ecc11..747a0410c
    
    747a0410c Fix workflow step to request PR reviewers (#3948)
    5e6bfa08e Updating readme file for win22 version 20210819.7 (#3947)
    a2d76d2a0 Add Windows Server 2022 image templates (#3929)
    20584a439 win/selenium: Download selenium-server-standalone.jar from GitHub (#3940)
    c81312dfa selenium: Download selenium-server-standalone.jar from GitHub releases (#3939)
    accd6d027 Add configure-tccdb-macos11 script (#3933)
    
    git-subtree-dir: .virtual-environments
    git-subtree-split: 747a0410cdad4831c6aef2ccf23b95ac69c25c8b
    
    * Updated vm image creation. Pretty path works
    
    * Squashed '.virtual-environments/' changes from 747a0410c..22087b3c7
    
    22087b3c7 [Windows] Add Visual Studio component (#3946)
    be3b04122 Update readme with Windows Server 2022 info (#3951)
    
    git-subtree-dir: .virtual-environments
    git-subtree-split: 22087b3c7a47428a189641f863bcf8ed8f142ace

---
+++ scripts/New-VMImageDeploy.ps1
@@ -0,0 +1,17 @@
+[CmdletBinding()]
+param(
+    [String] [Parameter (Mandatory=$true)] $ResourceGroupName,
+    [String] [Parameter (Mandatory=$true)] $Location,
+    [String] [Parameter (Mandatory=$true)] $TemplateFilePath,
+    [String] [Parameter (Mandatory=$true)] $TemplateParameterFilePath
+)
+
+az group create --name $ResourceGroupName --location $Location
+
+az deployment group create `
+    --resource-group $ResourceGroupName `
+    --template-file $TemplateFilePath `
+    --parameters $TemplateParameterFilePath `
+    --parameters adminPublicKey='$adminPublicKey'
+
+    
