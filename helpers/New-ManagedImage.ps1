fb4d67a22fa4dc3df0f347b310e77a67f45c4fba
Mike Johnson <mijohns@microsoft.com>
2021-12-06 12:48:32 -0700

    scrubbed

--- helpers/New-ManagedImage.ps1
+++ helpers/New-ManagedImage.ps1
@@ -1,6 +1,6 @@
 
 # https://stagentimage001.blob.core.windows.net/system/Microsoft.Compute/Images/images/packer-osDisk.1f058594-eabc-4cc1-8066-bfca700264ef.vhd
-az storage blob show --container-name images --account-name stagentimage001 --name pkros10hlu5rula.vhd
+az storage blob show --container-name images --account-name stagentimage001 --name <uniqueid>.vhd
 
 $storageAccountName = "stagentimage001"
 $containerName = "system"
@@ -9,5 +9,5 @@ $vhdBlobName = "packer-osDisk.1f058594-eabc-4cc1-8066-bfca700264ef"
 $blobUrl = "https://{0}.blob.core.windows.net/{1}/{2}/{3}.vhd" -f  $storageAccountName, $containerName, $containerPath, $vhdBlobName
 
 az disk create --resource-group msft-aware-imagegen-agent-ci --name $vhdBlobName --source $blobUrl
-az image create --resource-group msft-aware-imagegen-agent-ci --name $vhdBlobName --os-type Windows --source /subscriptions/cbadc96b-2923-4459-bb2d-b237af7f84d6/resourceGroups/msft-aware-imagegen-agent-ci/providers/Microsoft.Compute/disks/packer-osDisk.1f058594-eabc-4cc1-8066-bfca700264ef
+az image create --resource-group msft-aware-imagegen-agent-ci --name $vhdBlobName --os-type Windows --source /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/msft-aware-imagegen-agent-ci/providers/Microsoft.Compute/disks/packer-osDisk.1f058594-eabc-4cc1-8066-bfca700264ef
 Write-Host "stop"
