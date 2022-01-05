04aade2d80ec7574753a9a21a76f68191ca23ec4
Mike Johnson <mijohns@microsoft.com>
2022-01-05 11:23:03 -0700

    Update from actions/virtual-environments
    Updated using subtree from main from
    commit 3c2f1c6350b3d34a557f12f62e3ee47a839ea401

--- .virtual-environments/images/linux/scripts/installers/preimagedata.sh
+++ .virtual-environments/images/linux/scripts/installers/preimagedata.sh
@@ -1,30 +1,28 @@
 #!/bin/bash -e
+################################################################################
+##  File: post-deployment.sh
+##  Desc: Post deployment actions
+################################################################################
 
-imagedata_file=$IMAGEDATA_FILE
-image_version=$IMAGE_VERSION
-os_name=$(lsb_release -ds | sed "s/ /\\\n/g")
-image_label="ubuntu-$(lsb_release -rs)"
-github_url="https://github.com/actions/virtual-environments/blob"
+mv -f /imagegeneration/post-generation /opt
 
-if [[ "$image_label" =~ "ubuntu-20" ]]; then
-    software_url="${github_url}/ubuntu20/${image_version}/images/linux/Ubuntu2004-Readme.md"
-    releaseUrl="https://github.com/actions/virtual-environments/releases/tag/ubuntu20%2F${image_version}"
-fi
+echo "chmod -R 777 /opt"
+chmod -R 777 /opt
+echo "chmod -R 777 /usr/share"
+chmod -R 777 /usr/share
 
-if [[ "$image_label" =~ "ubuntu-18" ]]; then
-    software_url="${github_url}/ubuntu18/${image_version}/images/linux/Ubuntu1804-Readme.md"
-    releaseUrl="https://github.com/actions/virtual-environments/releases/tag/ubuntu18%2F${image_version}"
-fi
+# remove installer and helper folders
+rm -rf $HELPER_SCRIPT_FOLDER
+rm -rf $INSTALLER_SCRIPT_FOLDER
+chmod 755 $IMAGE_FOLDER
 
-cat <<EOF > $imagedata_file
-[
-  {
-    "group": "Operating System",
-    "detail": "${os_name}"
-  },
-  {
-    "group": "Virtual Environment",
-    "detail": "Environment: ${image_label}\nVersion: ${image_version}\nIncluded Software: ${software_url}\nImage Release: ${releaseUrl}"
-  }
-]
-EOF
+# Remove quotes around PATH
+ENVPATH=$(grep 'PATH=' /etc/environment | head -n 1 | sed -z 's/^PATH=*//')
+ENVPATH=${ENVPATH#"\""}
+ENVPATH=${ENVPATH%"\""}
+echo "PATH=$ENVPATH" | sudo tee -a /etc/environment
+echo "Updated /etc/environment: $(cat /etc/environment)"
+
+# Clean yarn and npm cache
+yarn cache clean
+npm cache clean --force
--- vm-images/images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
+++ vm-images/images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
@@ -140,7 +140,7 @@ function Get-HomebrewVersion {
 }
 
 function Get-CpanVersion {
-    $result = Get-CommandResult "cpan --version"
+    $result = Get-CommandResult "cpan --version" -ExpectExitCode @(25, 255)
     $result.Output -match "version (?<version>\d+\.\d+) " | Out-Null
     $cpanVersion = $Matches.version
     return "cpan $cpanVersion"
@@ -172,6 +172,11 @@ function Get-NpmVersion {
 function Get-YarnVersion {
     $yarnVersion = yarn --version
     return "Yarn $yarnVersion"
+}
+
+function Get-ParcelVersion {
+    $parcelVersion = parcel --version
+    return "Parcel $parcelVersion"
 }
 
 function Get-PipVersion {
